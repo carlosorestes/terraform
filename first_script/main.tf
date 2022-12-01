f50662c3ab26219bca1c16c86fb2d756ba39d7d6
carlosmalaquias <carlosomalaquias@gmail.com>
2022-12-01 18:05:24 -0300

    Adajustment main.tf

--- first_script/main.tf
+++ first_script/main.tf
@@ -4,14 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "mqs-sample-bucket-terraform-programaticaly" {
   bucket = "mqs-sample-bucket-terraform-programaticaly-00"
+  acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
-
-resource "aws_s3_bucket_acl" "mqs_sample_bucket_terraform" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
 }
