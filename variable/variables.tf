6449d853109492eb230765f785880e019ab0a672
carlosmalaquias <carlosomalaquias@gmail.com>
2022-12-13 06:09:49 -0300

    working with variables and environment

--- variable/variables.tf
+++ variable/variables.tf
@@ -1,32 +1,31 @@
-variable aws_region {
+variable "aws_region" {
   type        = string
   default     = "sa-east-1"
   description = "description"
 }
 
-variable aws_profile {
+variable "aws_profile" {
   type        = string
   default     = "default"
   description = "description"
 }
 
-variable instance_ami {
+variable "instance_ami" {
   type        = string
-  default     = "ami-074dc0a6f6c764218"
+  default     = "ami-0b7101e993ea27f3a"
   description = ""
 }
 
-variable instance_type {
+variable "instance_type" {
   type        = string
-  default     = "t3.micro"
   description = ""
 }
 
-variable instance_tags {
-  type        = map(string)
-  default     = {
-    Name = "Ubuntu"
-    Project = "Curso AWS com Terraform"
+variable "instance_tags" {
+  type = map(string)
+  default = {
+    Name    = "Ubuntu/ Course terraform"
+    Project = "Course AWS with Terraform"
   }
-  description = ""
+  description = "Couse AWS with Terraform"
 }
